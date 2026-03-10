0b8abc1cc9535ed9e8e5e2ce91f77a6a54e3eb8f
prof-eduardo-santos <edu.sigma@hotmail.com>
2026-03-09 22:34:04 -0300

    First Commit Projeto Lanchonete PWA

---
+++ script.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+
+navigator.serviceWorker.register("service-worker.js")
+
+.then(() => console.log("Service Worker registrado"))
+
+.catch(err => console.log("Erro:", err));
+
+}
